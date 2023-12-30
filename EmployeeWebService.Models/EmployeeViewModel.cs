99061fb59980f40746d23478b524239abe4ee543
Fall <pilimecka@mail.ru>
2023-12-30 13:15:53 +0300

    Create Models

---
+++ EmployeeWebService.Models/EmployeeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWebService.Models;
+
+public class EmployeeViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Phone { get; set; }
+    public int CompanyId { get; set; }
+    public Passport Passport { get; set; }
+    public Department Department { get; set; }
+}
