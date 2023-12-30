c13b181b75bcc911635916fb8ffa28e46b2221a5
Fall <pilimecka@mail.ru>
2023-12-30 13:05:34 +0300

    Добавьте файлы проекта.

---
+++ EmployeeWebService.API/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebService.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    public EmployeeController()
+    {
+
+    }
+}
